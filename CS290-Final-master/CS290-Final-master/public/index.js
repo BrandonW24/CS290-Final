6212ea6cc8347cb848735f1d4836cfb2b0008f56
Brandon Withington <withingb@oregonstate.edu>
2018-12-03 22:14:02 -0800

    got the modal kind of working!!!!!!!!

--- CS290-Final-master/CS290-Final-master/public/index.js
+++ CS290-Final-master/CS290-Final-master/public/index.js
@@ -3,13 +3,15 @@ var modal = document.getElementById('modal-thing');
 
 var button = document.getElementById('modalButt');
 
-var closeModal = document.getElementsByClassName('closeBUTT');
+var closeModal = document.getElementsByClassName("closeBUTT")[0];
 
 button.addEventListener('click', openModal);
 
 closeModal.addEventListener('click', closeMod);
 
+window.addEventListener('click', outOftheModalClick);
 
+//Modal stuff
 function openModal(){
 
     modal.style.display = 'block';
@@ -19,5 +21,13 @@ function openModal(){
 function closeMod(){
 
     modal.style.display ='none';
+
+}
+
+function outOftheModalClick(e){
+
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
 
 }
