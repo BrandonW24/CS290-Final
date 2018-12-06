f67226de966652979a4558e35b5bdbf6744af16d
Brandon Withington <withingb@oregonstate.edu>
2018-12-06 03:51:42 -0800

    added listeners for the modal

--- CS290-Final-master/CS290-Final-master/public/index.js
+++ CS290-Final-master/CS290-Final-master/public/index.js
@@ -1,17 +1,18 @@
+
 //modal variables
 var modal = document.getElementById('modal-thing');
-
 var button = document.getElementById('modalButt');
-
 var closeModal = document.getElementsByClassName("closeBUTT")[0];
-
 button.addEventListener('click', openModal);
-
 closeModal.addEventListener('click', closeMod);
-
 window.addEventListener('click', outOftheModalClick);
 
-//Modal stuff
+//variables to get user's input
+var postCategory = document.getElementById("post-category-fieldset");
+var userInputPhotoURL = document.getElementById("photoURLinput");
+var userInputTextPost = document.getElementById("userInpText");
+
+//******************************Modal listener stuff*********************
 function openModal(){
 
     modal.style.display = 'block';
@@ -31,3 +32,27 @@ function outOftheModalClick(e){
     }
 
 }
+
+//******************************End of Modal listener stuff*********************
+
+//***************Text Input listener things *************************************//
+postCategory.addEventListener('click', function(){
+    console.log("==A Category was selected");
+
+});
+
+userInputPhotoURL.addEventListener('keyup', function(ui){
+
+    console.log("The user typed something ");
+    userInputPhotoURL = ui.target.value.toLowerCase();
+    console.log(" User input here : ", userInputPhotoURL);
+
+});
+
+userInputTextPost.addEventListener('keyup', function (ui) {
+
+    console.log("The user typed something ");
+    userInputTextPost = ui.target.value.toLowerCase();
+    console.log(" User input here : ", userInputTextPost);
+
+});
